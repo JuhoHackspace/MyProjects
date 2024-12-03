3436e0fcabdbc3dea9189af9fa5c588606973d12
Juho Hietala <juhohietala90@gmail.com>
2024-12-03 14:31:24 +0200

    Boulder historia näkyy, karvalakkimallina vielä

--- HuiputinPaivakirja/src/screens/BoulderHistoryScreen.js
+++ HuiputinPaivakirja/src/screens/BoulderHistoryScreen.js
@@ -1,16 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import styles from '../styles/Styles'
 import { useTheme } from 'react-native-paper'
+import LoadingIcon from '../components/LoadingIcon'
+import { retrieveBoulderHistory } from '../firebase/FirebaseMethods'
 
 export default function BoulderHistoryScreen() {
   const { colors, fonts } = useTheme()
+  const [history, setHistory] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchHistory = async () => {
+        const data = await retrieveBoulderHistory();
+        if (data) {
+            // Sort the sends list by sentAt field in descending order
+            const sortedSends = data.sort((a, b) => new Date(b.send.sentAt) - new Date(a.send.sentAt));
+            // Take the last five sends
+            const lastFiveSends = sortedSends.slice(0, 5);
+            setHistory(lastFiveSends);
+            setLoading(false);
+        }
+    };
+
+    fetchHistory();
+  }, []);
+
+  const formatDate = (isoString) => {
+    const date = new Date(isoString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  };
+
+  if (loading) {
+    return (
+        <LoadingIcon/>
+    )
+  }
 
   return (
     <View style={styles.screenBaseContainer}>
       <View style={styles.headerContainer}>
         <Text style={{fontFamily: fonts.special.fontFamily, fontSize: 28 }}>Boulder History</Text>
       </View>
+      <FlatList
+        data={history}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <View style={styles.inputContainer}>
+            <Text style={styles.basicText}>Route: {history[index].route.routeName}</Text>
+            <Text style={styles.basicText}>Grade: {history[index].route.votedGrade}</Text>
+            <Text style={styles.basicText}>Sent At: {formatDate(history[index].send.sentAt)}</Text>
+            <Text style={styles.basicText}>Tries: {history[index].send.tries}</Text>
+          </View>
+        )}
+      />
     </View>
   )
 }
