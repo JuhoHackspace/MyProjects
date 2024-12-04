13b4025e8ef34020f35a4225c8e4fbc31d027ec9
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2024-12-04 21:30:27 +0200

    muutettu modalview komponenttia teemaan vaihtamista varten. Poistettu modalstylesistä kaikki turhat värit

--- HuiputinPaivakirja/src/components/ModalView.js
+++ HuiputinPaivakirja/src/components/ModalView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Modal, View, Text, TouchableOpacity, TextInput } from 'react-native';
 import modalStyles from '../styles/ModalStyles';
+import { useTheme } from 'react-native-paper';
 
 export default function ModalView({
   visible,
@@ -10,6 +11,8 @@ export default function ModalView({
   password,
   setPassword,
 }) {
+  const { colors } = useTheme();
+
   return (
     <Modal
       animationType="slide"
@@ -18,28 +21,29 @@ export default function ModalView({
       onRequestClose={onClose}
     >
       <View style={modalStyles.centeredView}>
-        <View style={modalStyles.modalView}>
+        <View style={[modalStyles.modalView, { backgroundColor: colors.background }]}>
           {deleteConfirmation ? (
             <>
-              <Text style={modalStyles.modalText}>
+              <Text style={[modalStyles.modalText, { color: colors.text }]}>
                 Delete your account and all your related information?
               </Text>
               <TextInput
-                style={modalStyles.input}
+                style={[modalStyles.input, { color: colors.text, borderColor: colors.text }]}
                 placeholder="Enter your password"
+                placeholderTextColor={colors.text}
                 secureTextEntry
                 value={password}
                 onChangeText={setPassword}
               />
               <View style={modalStyles.modalButtonContainer}>
                 <TouchableOpacity
-                  style={modalStyles.modalButtonDanger}
+                  style={[modalStyles.modalButtonDanger, { backgroundColor: colors.accent }]}
                   onPress={onDelete}
                 >
                   <Text style={modalStyles.modalButtonText}>Delete</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
-                  style={modalStyles.modalButton}
+                  style={[modalStyles.modalButton, { backgroundColor: colors.accent2 }]}
                   onPress={onClose}
                 >
                   <Text style={modalStyles.modalButtonText}>Cancel</Text>
@@ -48,9 +52,9 @@ export default function ModalView({
             </>
           ) : (
             <>
-              <Text style={modalStyles.modalText}>Modal!</Text>
+              <Text style={[modalStyles.modalText, { color: colors.text }]}>Modal!</Text>
               <TouchableOpacity
-                style={modalStyles.modalButton}
+                style={[modalStyles.modalButton, { backgroundColor: colors.accent }]}
                 onPress={onClose}
               >
                 <Text style={modalStyles.modalButtonText}>Close</Text>
