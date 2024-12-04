24195d8a0b15b5c02b90c39ee95fa46051675009
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2024-12-04 23:17:50 +0200

    sekoilu jatkuu

--- HuiputinPaivakirja/src/components/ColorPicker.js
+++ HuiputinPaivakirja/src/components/ColorPicker.js
@@ -3,7 +3,7 @@ import { Picker } from '@react-native-picker/picker';
 import styles from '../styles/Styles'
 import { useCustomTheme } from '../theme/CustomTheme';
 import { useTheme } from 'react-native-paper';
-import { Platform } from 'react-native';
+import { View } from 'react-native';
 
 export default function ColorPicker({value, setValue, isGrade}) {
   const { colors } = useTheme();
@@ -17,76 +17,73 @@ export default function ColorPicker({value, setValue, isGrade}) {
   }, [value, setValue]);
   
   return (
-    <Picker
-        selectedValue={value}
-        style={[styles.input, { 
-          backgroundColor: colors.background,
-          color: colors.text,
-          borderColor: colors.text,
-          borderWidth: 1,
-          borderRadius: 5,
-          marginVertical: 5,
-        }]}
-        dropdownIconColor={colors.text}
-        onValueChange={(itemValue) => setValue(itemValue)}
-        mode={Platform.OS === 'android' ? 'dropdown' : 'dialog'}
-    >
-        <Picker.Item 
-          label="Yellow" 
-          value="yellow" 
-          color={isDarkTheme ? '#ffffff' : '#000000'} 
-          style={{backgroundColor: colors.background}}
-        />
-        <Picker.Item 
-          label="Green" 
-          value="green" 
-          color={isDarkTheme ? '#ffffff' : '#000000'} 
-          style={{backgroundColor: colors.background}}
-        />
-        <Picker.Item 
-          label="Blue" 
-          value="blue" 
-          color={isDarkTheme ? '#ffffff' : '#000000'} 
-          style={{backgroundColor: colors.background}}
-        />
-        <Picker.Item 
-          label="Pink" 
-          value="pink" 
-          color={isDarkTheme ? '#ffffff' : '#000000'} 
-          style={{backgroundColor: colors.background}}
-        />
-        {!isGrade && (
+    <View style={{ 
+      backgroundColor: colors.background,
+      borderColor: colors.text,
+      borderWidth: 1,
+      borderRadius: 5,
+      marginVertical: 5,
+    }}>
+      <Picker
+          selectedValue={value}
+          style={[styles.input, { 
+            backgroundColor: colors.background,
+            color: colors.text,
+            margin: 0,
+            padding: 0,
+          }]}
+          dropdownIconColor={colors.text}
+          onValueChange={(itemValue) => setValue(itemValue)}
+          itemStyle={{ backgroundColor: colors.background }}
+      >
           <Picker.Item 
-            label="Orange" 
-            value="orange" 
+            label="Yellow" 
+            value="yellow" 
             color={isDarkTheme ? '#ffffff' : '#000000'} 
-            style={{backgroundColor: colors.background}}
           />
-        )}
-        <Picker.Item 
-          label="Red" 
-          value="red" 
-          color={isDarkTheme ? '#ffffff' : '#000000'} 
-          style={{backgroundColor: colors.background}}
-        />
-        <Picker.Item 
-          label="Purple" 
-          value="purple" 
-          color={isDarkTheme ? '#ffffff' : '#000000'} 
-          style={{backgroundColor: colors.background}}
-        />
-        <Picker.Item 
-          label="Black" 
-          value="black" 
-          color={isDarkTheme ? '#ffffff' : '#000000'} 
-          style={{backgroundColor: colors.background}}
-        />
-        <Picker.Item 
-          label="White" 
-          value="white" 
-          color={isDarkTheme ? '#ffffff' : '#000000'} 
-          style={{backgroundColor: colors.background}}
-        />
-    </Picker>
+          <Picker.Item 
+            label="Green" 
+            value="green" 
+            color={isDarkTheme ? '#ffffff' : '#000000'} 
+          />
+          <Picker.Item 
+            label="Blue" 
+            value="blue" 
+            color={isDarkTheme ? '#ffffff' : '#000000'} 
+          />
+          <Picker.Item 
+            label="Pink" 
+            value="pink" 
+            color={isDarkTheme ? '#ffffff' : '#000000'} 
+          />
+          {!isGrade && (
+            <Picker.Item 
+              label="Orange" 
+              value="orange" 
+              color={isDarkTheme ? '#ffffff' : '#000000'} 
+            />
+          )}
+          <Picker.Item 
+            label="Red" 
+            value="red" 
+            color={isDarkTheme ? '#ffffff' : '#000000'} 
+          />
+          <Picker.Item 
+            label="Purple" 
+            value="purple" 
+            color={isDarkTheme ? '#ffffff' : '#000000'} 
+          />
+          <Picker.Item 
+            label="Black" 
+            value="black" 
+            color={isDarkTheme ? '#ffffff' : '#000000'} 
+          />
+          <Picker.Item 
+            label="White" 
+            value="white" 
+            color={isDarkTheme ? '#ffffff' : '#000000'} 
+          />
+      </Picker>
+    </View>
   )
 }
