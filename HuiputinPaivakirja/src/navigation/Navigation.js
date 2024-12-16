3ff493b60fd73640a48a885259475cb3fd00ac4f
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2024-12-16 10:22:54 +0200

    Statusbar korjaus oli hukkunut jonnekkin matkalla niin palautin sen nyt tänne

--- HuiputinPaivakirja/src/navigation/Navigation.js
+++ HuiputinPaivakirja/src/navigation/Navigation.js
@@ -1,51 +1,75 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-
+import { StatusBar } from 'react-native';
 import { useAuth } from '../firebase/AuthProvider';
 import CustomDrawerContent from './CustomDrawerContent';
 import AuthStackNavigator from './AuthStackNavigator';
 import MainStackNavigator from './MainStackNavigator';
 import LoadingIcon from '../components/LoadingIcon';
+import { useCustomTheme } from '../theme/CustomTheme';
+import { useTheme } from 'react-native-paper';
 
 const Drawer = createDrawerNavigator();
 
 function AppNavigator() {
   const { user, loading } = useAuth();
+  const { isDarkTheme } = useCustomTheme();
+  const { colors } = useTheme();
 
   if (loading) {
     // Show loading indicator while checking if user is logged in
     return (
-      <LoadingIcon />
+      <>
+        <StatusBar
+          backgroundColor={colors.background}
+          barStyle={isDarkTheme ? "light-content" : "dark-content"}
+        />
+        <LoadingIcon />
+      </>
     );
   }
 
   if (!user) {
     // When user is not logged in, show AuthStackNavigator
-    return <AuthStackNavigator />;
+    return (
+      <>
+        <StatusBar
+          backgroundColor={colors.background}
+          barStyle={isDarkTheme ? "light-content" : "dark-content"}
+        />
+        <AuthStackNavigator />
+      </>
+    );
   }
 
   // User logged in, show MainStackNavigator and Drawer
   return (
-    <Drawer.Navigator
-      drawerContent={(props) => <CustomDrawerContent {...props} />}
-      screenOptions={{
-        headerShown: false,
-        drawerPosition: 'right',
-        swipeEnabled: false, // Disable drawer gesture
-        gestureEnabled: false // Disable all drawer gestures
-      }}
-    >
-      <Drawer.Screen name="MainStack" component={MainStackNavigator} />
-    </Drawer.Navigator>
+    <>
+      <StatusBar
+        backgroundColor={colors.background}
+        barStyle={isDarkTheme ? "light-content" : "dark-content"}
+      />
+      <Drawer.Navigator
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+        screenOptions={{
+          headerShown: false,
+          drawerPosition: 'right',
+          swipeEnabled: false, // Disable drawer gesture
+          gestureEnabled: false // Disable all drawer gestures
+        }}
+      >
+        <Drawer.Screen name="MainStack" component={MainStackNavigator} />
+      </Drawer.Navigator>
+    </>
   );
 }
 
 function Navigation() {
   return (
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
 
